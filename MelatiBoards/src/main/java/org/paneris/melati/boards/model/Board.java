eb92d9d129638574e0bdfe7b646bbcd4d1407ad4
Myles Chippendale <mylesc@paneris.org>
2001-05-03 18:01:28 +0000

    Refactoring

--- MelatiBoards/src/main/java/org/paneris/melati/boards/model/Board.java
+++ MelatiBoards/src/main/java/org/paneris/melati/boards/model/Board.java
@@ -55,11 +55,13 @@ import org.paneris.melati.boards.model.UserTable;
 import org.paneris.melati.boards.model.generated.*;
 import java.io.*;
 import java.util.*;
+import java.lang.ref.*;
 import java.sql.Date;
 import java.sql.Timestamp;
 import javax.swing.tree.DefaultMutableTreeNode;
 import org.melati.*;
 import org.melati.poem.*;
+import org.melati.template.*;
 import org.melati.util.*;
 
 public class Board extends BoardBase {
@@ -67,7 +69,6 @@ public class Board extends BoardBase {
 
   /***************************
    * Users levels - Admin, Manager, Member
-   *                Checks, lists and counts
    ****************************/
 
   public boolean isAdmin(User user) {
@@ -211,6 +212,14 @@ public class Board extends BoardBase {
         throw new AccessPoemException(token, new Capability("Logged In"));
     }
   }
+
+  // method to allow a board to be created with a different manager than the
+  // creating user
+  public final void makePersistent(User manager) {
+    ((BoardTable)getTable()).create(this, manager);
+  }
+
+
   /***************************
    * Subscribing/Unsubscribing
    ***************************/
@@ -368,14 +377,14 @@ public class Board extends BoardBase {
     return (ChildrenDrivenMutableTree)threadsByRoot.get(m);
   }
 
-  Vector messages = null;
+  SoftReference messages = null;
 
   // Enumeration of DefaultMutableTreeNodes
   private void computeMessages() {
     if (threadsByRoot == null)
       computeThreads();
 
-    messages = new Vector();
+    Vector realMessages = new Vector();
     for(int i=0; i < threadTrees.size(); i++)
     {
       ChildrenDrivenMutableTree thread =
@@ -383,14 +392,18 @@ public class Board extends BoardBase {
       Enumeration e = thread.preorderEnumeration();
 //      Enumeration e = thread.breadthFirstEnumeration();
       while (e.hasMoreElements())
-        messages.addElement(e.nextElement());
+        realMessages.addElement(e.nextElement());
     }
+
+    messages = new SoftReference(realMessages);
   }
 
+
   public Enumeration getMessages() {
-    if (messages == null)
+    if (messages == null || messages.get() == null)
       computeMessages();
-    return messages.elements();
+    Vector realMessages = (Vector)messages.get();
+    return (realMessages == null) ? null : realMessages.elements();
   }
 
   private CachedCount messageCount = null;
@@ -405,6 +418,8 @@ public class Board extends BoardBase {
   /********************
    * Management stuff
    ********************/
+
+  /* Pending Messages */
 
   private CachedCount pendingMessageCount = null;
   private CachedSelection pendingMessages = null;
@@ -428,6 +443,8 @@ public class Board extends BoardBase {
                                     cachedPendingMessages(this);
     return pendingMessages.objects();
   }
+
+  /* Pending Subscriptions */
 
   private CachedCount pendingSubscriptionCount = null;
   private CachedSelection pendingSubscriptions = null;
@@ -454,7 +471,7 @@ public class Board extends BoardBase {
 
 
   /********************************
-   * Notes about the list for users
+   * Some board constants
    ********************************/
 
   public String getEmailAddress() throws SettingNotFoundException {
@@ -481,70 +498,6 @@ public class Board extends BoardBase {
   public String templatePath(String templateName) {
     return getBoardTable().getBoardsEmailTemplates() + File.separator +
              templateName + ".wm";
-  }
-/*
-  public String evalTemplate(Melati melati, String template) {
-    // construct a Melati with a StringWriter instead of a servlet
-    // request and response
-    TemplateEngine = melati.getTemplateEngine();
-    MelatiWriter sw = TemplateEngine.getStringWriter(melati.getEncoding());
-    Melati melati2 = new Melati(melati.getConfig(), sw);
-    TemplateContext context = templateEngine.getTemplateContext(melati2);
-    context.put("melati", melati2);
-    templateEngine.expandTemplate(melati2.getWriter(),
-                                  templatePath(template),
-                                  context);
-    return sw.asString();
-  }
-
-  public String getSubscribedNote(Melati melati) {
-    return evalTemplate(melati, "SubscribedNote");
-    return "You are now subscribed to the "+getDisplayname()+
-           " board. Please post messages to "+getEmailAddress(); // FIXME
-  }
-
-  public String getSubscriptionRequestReceivedNote() {
-    return "We have received your request to join the  "+getDisplayname()+
-           " board. We will let you know if you have been accepted.";
-  }
-
-  public String getSubscriptionRequestApprovedNote() {
-    return "We have received your request to join the  "+getDisplayname()+
-           " board. We will let you know if you have been accepted.";
-  }
-
-  public String getSubscriptionRequestDeclinedNote() {
-    String note = "Unfortunately, your request to join the  "+getDisplayname()+
-                  " board has been declined.\nPlease contact one of the managers"+
-                  " for more information. The managers are:\n";
-    Enumeration e = getManagers();
-    User manager = null;
-    while(e.hasMoreElements()) {
-      manager = (User)e.nextElement();
-      note += "  " + manager.getName() + " : " + manager.getEmail() + "\n";
-    }
-    return note;
-    // Delete the subscription
-  }
-
-  public String getUsageInstructionsNote() {
-    return "You should visit the web site to do most stuff " +
-           "or just reply to messages"; //FIXME
-  }
-*/
-  public String getManageInstructionsNote() {
-    return "You should visit the web site to do most stuff " +
-           "or just reply to messages"; //FIXME
-  }
-
-  public String getThisOrganisationNote() {
-      return "Welcome to Paneris!!!"; //FIXME
-  }
-  
-  // method to allow a board to be created with a different manager than the
-  // creating user
-  public final void makePersistent(User manager) {
-    ((BoardTable)getTable()).create(this, manager);
   }
 
 }
