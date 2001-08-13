b74df262c8037611f03c69210973992c27d1b2b8
Tim Joyce <timj@paneris.org>
2001-08-13 11:24:23 +0000

    ensure that the author of the parent message is coppied on the reply if they are nto subscribed

--- MelatiBoards/src/main/java/org/paneris/melati/boards/model/Board.java
+++ MelatiBoards/src/main/java/org/paneris/melati/boards/model/Board.java
@@ -66,215 +66,221 @@ import org.melati.util.*;
 
 public class Board extends BoardBase {
   public Board() {}
-
+  
   /***************************
    * Users levels - Admin, Manager, Member
    ****************************/
-
+  
   public boolean isAdmin(User user) {
     return ((AccessToken)user).givesCapability(
-                                 getDatabase().administerCapability());
+    getDatabase().administerCapability());
   }
-
+  
   public boolean isManager(User user) {
     return getBoardsDatabaseTables().getSubscriptionTable().
-             isManager(user, this);
+    isManager(user, this);
   }
-
+  
   public boolean isMember(User user) {
     return getBoardsDatabaseTables().getSubscriptionTable().
-             isMember(user, this);
+    isMember(user, this);
   }
-
+  
   /***************************
    * List users
    ****************************/
-
+  
   private CachedCount managerCount = null;
   
   public int getManagerCount() {
     if (managerCount == null)
       managerCount =
-        getBoardsDatabaseTables().getSubscriptionTable().
-          cachedManagerCount(this);
+      getBoardsDatabaseTables().getSubscriptionTable().
+      cachedManagerCount(this);
     return managerCount.count();
   }
-
+  
   private CachedSelection managersubs = null;
   
   public Enumeration getManagersSubscriptions() {
     if (managersubs == null)
       managersubs =
-        getBoardsDatabaseTables().getSubscriptionTable().cachedManagers(this);
+      getBoardsDatabaseTables().getSubscriptionTable().cachedManagers(this);
     return managersubs.objects();
   }
-
+  
   public Enumeration getManagers() {
     return new MappedEnumeration(getManagersSubscriptions()) {
-             public Object mapped(Object subscription) {
-               return ((Subscription)subscription).getUser();
-             }
-           };
+      public Object mapped(Object subscription) {
+        return ((Subscription)subscription).getUser();
+      }
+    };
   }
-
+  
   private CachedCount memberCount = null;
   
   public int getMemberCount() {
     if (memberCount == null)
       memberCount =
-        getBoardsDatabaseTables().getSubscriptionTable().
-                                    cachedMemberCount(this);
+      getBoardsDatabaseTables().getSubscriptionTable().
+      cachedMemberCount(this);
     return memberCount.count();
   }
-
+  
   private CachedSelection membersubs = null;
   
   public Enumeration getMembersSubscriptions() {
     if (membersubs == null)
       membersubs =
-        getBoardsDatabaseTables().getSubscriptionTable().cachedMembers(this);
+      getBoardsDatabaseTables().getSubscriptionTable().cachedMembers(this);
     return membersubs.objects();
   }
-
+  
   public Enumeration getMembers() {
     return new MappedEnumeration(getMembersSubscriptions()) {
-             public Object mapped(Object subscription) {
-               return ((Subscription)subscription).getUser();
-             }
-           };
+      public Object mapped(Object subscription) {
+        return ((Subscription)subscription).getUser();
+      }
+    };
   }
-
+  
   public Enumeration getNormalDistributionList() {
     return getBoardsDatabaseTables().getSubscriptionTable().
-                             getNormalDistributionList(this);
+    getNormalDistributionList(this);
   }
-
+  
   public Enumeration getDigestDistributionList() {
     return getBoardsDatabaseTables().getSubscriptionTable().
-                             getDigestDistributionList(this);
+    getDigestDistributionList(this);
   }
-
+  
   /*************************
    * Actions permitted to users of this board
    **************************/
-
+  
   public boolean canSubscribe(User user) {
     return  getOpensubscription().booleanValue() ||
-            isManager(user) ||
-            isAdmin(user);
+    isManager(user) ||
+    isAdmin(user);
   }
-
+  
   public boolean canPost(User user) {
     return getOpenposting().booleanValue() ||
-           isMember(user) ||
-           isAdmin(user);
+    isMember(user) ||
+    isAdmin(user);
   }
-
+  
   public boolean canViewMessages(User user) {
     return getOpenmessageviewing().booleanValue() ||
-           isMember(user) ||
-           isAdmin(user);
+    isMember(user) ||
+    isAdmin(user);
   }
-
+  
   public boolean canViewMembers(User user) {
     return getOpenmemberlist().booleanValue() ||
-           isMember(user) ||
-           isAdmin(user);
+    isMember(user) ||
+    isAdmin(user);
   }
-
+  
   public boolean canManage(User user) {
     return isManager(user) ||
-           isAdmin(user);
+    isAdmin(user);
   }
-
-
+  
+  
   /*********************
    * Creating/Updating a board
    *********************/
-
+  
   public void assertCanWrite(AccessToken token)
-      throws AccessPoemException {
+  throws AccessPoemException {
     if (token == AccessToken.root) return;
     try {
       if (!canManage((User)token))
         throw new AccessPoemException(token, new Capability("Manager"));
     } catch (ClassCastException e) {
-        throw new AccessPoemException(token, new Capability("Manager"));
+      throw new AccessPoemException(token, new Capability("Manager"));
     }
   }
   public void assertCanCreate(AccessToken token)
-      throws AccessPoemException {
+  throws AccessPoemException {
     if (token == AccessToken.root) return;
     try {
       if (token == getDatabase().guestAccessToken())
         throw new AccessPoemException(token, new Capability("Logged In"));
     } catch (ClassCastException e) {
-        throw new AccessPoemException(token, new Capability("Logged In"));
+      throw new AccessPoemException(token, new Capability("Logged In"));
     }
   }
-
+  
   // method to allow a board to be created with a different manager than the
   // creating user
   public final void makePersistent(User manager) {
     ((BoardTable)getTable()).create(this, manager);
   }
-
-
+  
+  
   /***************************
    * Subscribing/Unsubscribing
    ***************************/
-
+  
   public void subscribe(User user, MembershipStatus status,
-                        Boolean ismanager, Boolean approved) {
+  Boolean ismanager, Boolean approved) {
     Subscription sub =
-      getBoardsDatabaseTables().getSubscriptionTable().
-        subscribe(user, this, status, ismanager, approved);
-
+    getBoardsDatabaseTables().getSubscriptionTable().
+    subscribe(user, this, status, ismanager, approved);
+    
     if (sub.getApproved_unsafe() == Boolean.FALSE) {
-       // FIXME
-       // send getSubscriptionRequestReceivedNote() message to user
+      // FIXME
+      // send getSubscriptionRequestReceivedNote() message to user
     }
   }
-
+  
   public void subscribe(User user) {
     subscribe(user,
-              getBoardsDatabaseTables().getMembershipStatusTable().
-                getNormal(),
-              Boolean.FALSE,
-              new Boolean(!getModeratedsubscription_unsafe().booleanValue()));
+    getBoardsDatabaseTables().getMembershipStatusTable().
+    getNormal(),
+    Boolean.FALSE,
+    new Boolean(!getModeratedsubscription_unsafe().booleanValue()));
   }
   
   public boolean isSubscribed(User user) {
     return getBoardsDatabaseTables().getSubscriptionTable().isMember(user, this);
   }
-
+  
   public void unsubscribe(User user) {
     getBoardsDatabaseTables().getSubscriptionTable().unsubscribe(user, this);
   }
-
+  
   public Subscription getUserSubscription(User user) {
     return getBoardsDatabaseTables().getSubscriptionTable().
-                             getUserSubscription(user, this);
+    getUserSubscription(user, this);
   }
-
-
+  
+  
   /****************************************
    * Distribute
    ****************************************/
-
+  
   public void distribute(Message message) {
-
+    
     // Vector of email addresses (Strings)
     Vector members = EnumUtils.vectorOf(
-                       new MappedEnumeration(getMembers()) {
-                         public Object mapped(Object member) {
-                           return ((User)member).getEmail();
-                         }
-                       });
-
+    new MappedEnumeration(getMembers()) {
+      public Object mapped(Object member) {
+        return ((User)member).getEmail();
+      }
+    });
+    
+    // include author of parent message if they are not subscribed to this board
+    // (ie board allows open posting)
+    Message parent = message.getParent();
+    if (parent != null && !isSubscribed(parent.getAuthor()))
+      members.add(parent.getAuthor().getEmail());
+    
     String[] emailArray = new String[members.size()];
     members.copyInto(emailArray);
-
+    
 /*
     String toString = "";
     for(int i=0; i<emailArray.length; i++) {
@@ -282,14 +288,10 @@ public class Board extends BoardBase {
     }
     if (toString.length() != 0)
       toString = toString.substring(0, toString.length() - 2);
-*/
-
+ */
+    
     String toString = getEmailAddress(message);
-    // include sender in replyto it they are not subscribed to this board
-    // (ie board allows open posting)
     String replyTo = toString;
-    if (!isSubscribed(message.getAuthor()))
-      replyTo += "," + message.getAuthor().getEmail();
     String body = message.getBody();
     body += "\n\n-----------------------------------------------------------------------\n";
     body += "From the " + getDisplayname() + " board:\n";
@@ -300,40 +302,40 @@ public class Board extends BoardBase {
       body += " (has "+message.getAttachmentCount()+" attachments)";
     body += ":\n";
     body += getMessageURL(message) + "\n";
-
+    
     try {
-        Email.sendToList(
-          getDatabase(),
-          message.getAuthor().getEmail(),   // From
-          emailArray,                       // To list
-          toString,                         // apparently to
-          replyTo,                          // reply to
-          "["+getName()+"] " + message.getSubject(),    // subject
-          body);
+      Email.sendToList(
+      getDatabase(),
+      message.getAuthor().getEmail(),   // From
+      emailArray,                       // To list
+      toString,                         // apparently to
+      replyTo,                          // reply to
+      "["+getName()+"] " + message.getSubject(),    // subject
+      body);
     } catch (Exception e) {
       e.printStackTrace();
     }
-
+    
   }
-
-
+  
+  
   /****************************************
    * Messages and Threads (stored as Trees)
    ****************************************/
-
+  
   private CachedSelection roots = null;
-
+  
   // Enumeration of Messages
   private Enumeration threadsRoots() {
     if (roots == null)
       roots = getBoardsDatabaseTables().getMessageTable().
-                                          cachedBoardRoots(this);
+      cachedBoardRoots(this);
     return roots.objects();
   }
-
+  
   private Vector threadTrees = null;
   private Hashtable threadsByRoot = null;
-
+  
   private void computeThreads() {
     threadTrees = new Vector();
     threadsByRoot = new Hashtable();
@@ -343,11 +345,11 @@ public class Board extends BoardBase {
       addThread(root);
     }
   }
-
+  
   void addThread(Message root) {
     addThread(root, false);
   }
-
+  
   void addThread(Message root, boolean atStart) {
     if (threadTrees == null)  // Don't bother is we haven't built the trees
       return;                 // (They will be built when we try to access them)
@@ -359,7 +361,7 @@ public class Board extends BoardBase {
     threadsByRoot.put(root, tree);
     computeMessages();
   }
-
+  
   void addToParent(Message mess, Message parent) {
     if (threadTrees == null)  // Don't bother if we haven't built the trees
       return;                 // (They will be built when we try to access them)
@@ -367,149 +369,149 @@ public class Board extends BoardBase {
     DefaultMutableTreeNode parentNode = getTreeNode(parent);
     if (parentNode == null)     // Shouldn't happen
       throw new MessageNotInBoardException("Message "+parent+
-                                           " is not in this board ("+this+")");
+      " is not in this board ("+this+")");
     parentNode.add(messNode);
     computeMessages();
   }
-
+  
   private DefaultMutableTreeNode getTreeNode(Message m) {
     DefaultMutableTreeNode res = null;
     for(int i=0; i < threadTrees.size(); i++) {
       ChildrenDrivenMutableTree thread =
-                           (ChildrenDrivenMutableTree)threadTrees.elementAt(i);
+      (ChildrenDrivenMutableTree)threadTrees.elementAt(i);
       if ((res = thread.getTreeNodeFor(m)) != null)
         return res;
     }
     return null;
   }
-
+  
   public ChildrenDrivenMutableTree threadWithRoot(Message m) {
     if (threadsByRoot == null)
       computeThreads();
     return (ChildrenDrivenMutableTree)threadsByRoot.get(m);
   }
-
+  
   SoftReference messages = null;
-
+  
   // Enumeration of DefaultMutableTreeNodes
   private void computeMessages() {
     if (threadsByRoot == null)
       computeThreads();
-
+    
     Vector realMessages = new Vector();
     for(int i=0; i < threadTrees.size(); i++)
     {
       ChildrenDrivenMutableTree thread =
-        (ChildrenDrivenMutableTree)threadTrees.elementAt(i);
+      (ChildrenDrivenMutableTree)threadTrees.elementAt(i);
       Enumeration e = thread.preorderEnumeration();
-//      Enumeration e = thread.breadthFirstEnumeration();
+      //      Enumeration e = thread.breadthFirstEnumeration();
       while (e.hasMoreElements())
         realMessages.addElement(e.nextElement());
     }
-
+    
     messages = new SoftReference(realMessages);
   }
-
-
+  
+  
   public Enumeration getMessages() {
     if (messages == null || messages.get() == null)
       computeMessages();
     Vector realMessages = (Vector)messages.get();
     return (realMessages == null) ? null : realMessages.elements();
   }
-
+  
   private CachedCount messageCount = null;
   
   public int getMessageCount() {
     if (messageCount == null)
       messageCount =
-        getBoardsDatabaseTables().getMessageTable().cachedMessageCount(this);
+      getBoardsDatabaseTables().getMessageTable().cachedMessageCount(this);
     return messageCount.count();
   }
-
+  
   /********************
    * Management stuff
    ********************/
-
+  
   /* Pending Messages */
-
+  
   private CachedCount pendingMessageCount = null;
   private CachedSelection pendingMessages = null;
-
+  
   public boolean hasPendingMessages() {
     return getPendingMessageCount() > 0;
   }
-
+  
   public int getPendingMessageCount() {
     if (pendingMessageCount == null)
       pendingMessageCount =
-        getBoardsDatabaseTables().getMessageTable().
-                                    cachedPendingMessageCount(this);
+      getBoardsDatabaseTables().getMessageTable().
+      cachedPendingMessageCount(this);
     return pendingMessageCount.count();
   }
-
+  
   public Enumeration getPendingMessages() {
     if (pendingMessages == null)
       pendingMessages =
-        getBoardsDatabaseTables().getMessageTable().
-                                    cachedPendingMessages(this);
+      getBoardsDatabaseTables().getMessageTable().
+      cachedPendingMessages(this);
     return pendingMessages.objects();
   }
-
+  
   /* Pending Subscriptions */
-
+  
   private CachedCount pendingSubscriptionCount = null;
   private CachedSelection pendingSubscriptions = null;
-
+  
   public boolean hasPendingSubscriptions() {
     return getPendingSubscriptionCount() > 0;
   }
-
+  
   public int getPendingSubscriptionCount() {
     if (pendingSubscriptionCount == null)
       pendingSubscriptionCount =
-        getBoardsDatabaseTables().getSubscriptionTable().
-                                    cachedPendingSubscriptionCount(this);
+      getBoardsDatabaseTables().getSubscriptionTable().
+      cachedPendingSubscriptionCount(this);
     return pendingSubscriptionCount.count();
   }
-
+  
   public Enumeration getPendingSubscriptions() {
     if (pendingSubscriptions == null)
       pendingSubscriptions =
-        getBoardsDatabaseTables().getSubscriptionTable().
-                                    cachedPendingSubscriptions(this);
+      getBoardsDatabaseTables().getSubscriptionTable().
+      cachedPendingSubscriptions(this);
     return pendingSubscriptions.objects();
   }
-
-
+  
+  
   /********************************
    * Some board constants
    ********************************/
-
+  
   public String getEmailAddress() throws SettingNotFoundException {
     return getName() + "@" + getBoardTable().getBoardsEmailDomain();
   }
-
+  
   public String getEmailAddress(Message m) throws SettingNotFoundException {
     return m.troid() + "." + getName() + "@" +
-           getBoardTable().getBoardsEmailDomain();
+    getBoardTable().getBoardsEmailDomain();
   }
-
+  
   public String getBoardURL() throws SettingNotFoundException {
     return getBoardTable().getBoardsSystemURL() + "/" +
-           getBoardTable().getLogicalDatabase() + "/board/" +
-           troid()+"/Board";
+    getBoardTable().getLogicalDatabase() + "/board/" +
+    troid()+"/Board";
   }
-
+  
   public String getMessageURL(Message m) throws SettingNotFoundException {
     return getBoardTable().getBoardsSystemURL() + "/" +
-           getBoardTable().getLogicalDatabase() + "/message/" +
-           m.troid()+"/Message";
+    getBoardTable().getLogicalDatabase() + "/message/" +
+    m.troid()+"/Message";
   }
-
+  
   public String templatePath(String templateName) {
     return getBoardTable().getBoardsEmailTemplates() + File.separator +
-             templateName + ".wm";
+    templateName + ".wm";
   }
-
+  
 }
