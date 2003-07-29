dbb46b9a6fc4b8dc0bb1e9bd55a9cd6329bb325f
Tim Pizey <timp21337@paneris.org>
2003-07-29 11:36:22 +0000

    Javadoc

--- MelatiBoards/src/main/java/org/paneris/melati/boards/receivemail/nntp/NNTPServer.java
+++ MelatiBoards/src/main/java/org/paneris/melati/boards/receivemail/nntp/NNTPServer.java
@@ -1,3 +1,19 @@
+/*
+ * $Source$
+ * $Revision$
+ *
+ * Copyright (C) 2003 Vasily Pozhidaev 
+ *
+ * Part of a Melati application. This application is free software;
+ * Permission is granted to copy, distribute and/or modify this
+ * software under the same terms as those set out for Melati at 
+ * http://melati.org.
+ *
+ * Contact details for copyright holder:
+ *
+ *     Vasily Pozhidaev  <vasilyp@paneris.org>
+ */
+
 package org.paneris.melati.boards.receivemail.nntp;
 
 import java.net.ServerSocket;
@@ -7,6 +23,15 @@ import javax.servlet.ServletException;
 
 import org.paneris.melati.boards.receivemail.Log;
 
+/**
+ * A News Server which enables read and write access to a 
+ * Board.
+ * Messages posted through the NNTP interface are distributed 
+ * to the subscribed users by email in the normal way. 
+ * 
+ * @author Vasily Pozhidaev <vasilyp@paneris.org>
+ *
+ */
 public class NNTPServer implements Runnable {
 
   private String identifier = null;
@@ -14,16 +39,21 @@ public class NNTPServer implements Runnable {
   private Properties config = null;
   private int bufSize = 65536;
   private Log log = null;
-  private NNTPStore _store = null;
   private Thread myThread = null;
 
-  public NNTPServer(
-    String identifier,
-    int port,
-    Properties config,
-    int bufSize,
-    Log log)
-    throws ServletException {
+  /**
+   * Constructor.
+   * 
+   * @param identifier  News group identifier eg org.paneris
+   * @param port        Socket port to listen on
+   * @param config      A properties file containing configuration
+   * @param bufSize     The Buffer size to use
+   * @param log         A Log object
+   * @throws ServletException if anything goes wrong
+   */
+  public NNTPServer(String identifier, int port, Properties config,
+                    int bufSize, Log log)
+      throws ServletException {
     this.identifier = identifier;
     this.port = port;
     this.config = config;
@@ -31,6 +61,11 @@ public class NNTPServer implements Runnable {
     this.log = log;
   }
 
+  /**
+   * Run it.
+   * 
+   * @see java.lang.Runnable#run()
+   */
   public void run() {
     myThread = Thread.currentThread();
 
@@ -46,6 +81,11 @@ public class NNTPServer implements Runnable {
     }
   }
 
+  /**
+   * Stop the server
+   * 
+   * @throws IllegalStateException if the server is not running.
+   */
   public void stop() throws IllegalStateException {
     if (myThread == null)
       throw new IllegalStateException("Server is not running");
