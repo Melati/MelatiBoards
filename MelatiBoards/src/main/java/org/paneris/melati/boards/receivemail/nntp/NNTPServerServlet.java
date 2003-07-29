2a93746083fba8010018a88a3ed5067942f4b6b3
Tim Pizey <timp21337@paneris.org>
2003-07-29 11:37:45 +0000

    Javadoc and comment out unused variable

--- MelatiBoards/src/main/java/org/paneris/melati/boards/receivemail/nntp/NNTPServerServlet.java
+++ MelatiBoards/src/main/java/org/paneris/melati/boards/receivemail/nntp/NNTPServerServlet.java
@@ -2,49 +2,16 @@
  * $Source$
  * $Revision$
  *
- * Copyright (C) 2000 Myles Chippendale
+ * Copyright (C) 2003 Vasily Pozhidaev 
  *
  * Part of a Melati application. This application is free software;
  * Permission is granted to copy, distribute and/or modify this
- * software under the same terms as those set out for Melati, below.
- *
- * Melati (http://melati.org) is a framework for the rapid
- * development of clean, maintainable web applications.
- *
- * Melati is free software; Permission is granted to copy, distribute
- * and/or modify this software under the terms either:
- *
- * a) the GNU General Public License as published by the Free Software
- *    Foundation; either version 2 of the License, or (at your option)
- *    any later version,
- *
- *    or
- *
- * b) any version of the Melati Software License, as published
- *    at http://melati.org
- *
- * You should have received a copy of the GNU General Public License and
- * the Melati Software License along with this program;
- * if not, write to the Free Software Foundation, Inc.,
- * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA to obtain the
- * GNU General Public License and visit http://melati.org to obtain the
- * Melati Software License.
- *
- * Feel free to contact the Developers of Melati (http://melati.org),
- * if you would like to work out a different arrangement than the options
- * outlined here.  It is our intention to allow Melati to be used by as
- * wide an audience as possible.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * software under the same terms as those set out for Melati at 
+ * http://melati.org.
  *
  * Contact details for copyright holder:
  *
- *     Mylesc Chippendale <mylesc@paneris.org>
- *     http://paneris.org/
- *     29 Stanley Road, Oxford, OX4 1QY, UK
+ *     Vasily Pozhidaev  <vasilyp@paneris.org>
  */
 
 package org.paneris.melati.boards.receivemail.nntp;
@@ -62,6 +29,12 @@ import javax.servlet.ServletResponse;
 
 import org.paneris.melati.boards.receivemail.Log;
 
+/**
+ * A servlet to run the NNTP Server.
+ * 
+ * @author vasilyp@paneris.org
+ *
+ */
 public class NNTPServerServlet implements Servlet {
 
   private ServletConfig config = null;
@@ -76,7 +49,7 @@ public class NNTPServerServlet implements Servlet {
   public void init(ServletConfig config) throws ServletException {
     this.config = config;
     String logPath = config.getInitParameter("log");
-    String pt = config.getInitParameter("port");
+//    String pt = config.getInitParameter("port");
     String database = config.getInitParameter("database");
     String prefix = config.getInitParameter("prefix");
     identifier = config.getInitParameter("identifier");
@@ -105,6 +78,9 @@ public class NNTPServerServlet implements Servlet {
     }
   }
 
+  /* 
+   * @see javax.servlet.Servlet#destroy()
+   */
   public void destroy() {
     try {
       nntpServer.interrupt();
@@ -114,14 +90,23 @@ public class NNTPServerServlet implements Servlet {
     }
   }
 
+  /* 
+   * @see javax.servlet.Servlet#getServletConfig()
+   */
   public ServletConfig getServletConfig() {
     return config;
   }
 
+  /* 
+   * @see javax.servlet.Servlet#getServletInfo()
+   */
   public String getServletInfo() {
     return "NNTP Server launcher for Melati boards system";
   }
 
+  /* 
+   * @see javax.servlet.Servlet#service(javax.servlet.ServletRequest, javax.servlet.ServletResponse)
+   */
   public void service(ServletRequest req, ServletResponse resp)
     throws ServletException, IOException {
     // Give info about the server
