6712eba3e3093ddf309b3a20171706f2bcb5becc
Tim Pizey <timp@paneris.org>
2005-03-31 12:56:52 +0000

    Use org.melati.util.UTF8Encoder

--- MelatiBoards/src/main/java/org/paneris/melati/boards/model/Attachment.java
+++ MelatiBoards/src/main/java/org/paneris/melati/boards/model/Attachment.java
@@ -53,11 +53,11 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URLEncoder;
 
 import org.melati.poem.AccessPoemException;
 import org.melati.poem.ValidationPoemException;
 import org.melati.util.UnexpectedExceptionException;
+import org.melati.util.UTF8URLEncoder;
 import org.paneris.melati.boards.model.generated.AttachmentBase;
 
 /**
@@ -66,6 +66,9 @@ import org.paneris.melati.boards.model.generated.AttachmentBase;
 public class Attachment extends AttachmentBase {
   public Attachment() {}
 
+  /* (non-Javadoc)
+   * @see org.paneris.melati.boards.model.generated.AttachmentBase#setFilename_unsafe(java.lang.String)
+   */
   public void setFilename_unsafe(String cooked) {
     super.setFilename_unsafe(cooked);
     Message thisMessage = (Message)getDatabase().getTable("message").
@@ -75,33 +78,47 @@ public class Attachment extends AttachmentBase {
     setPath_unsafe(board.getAttachmentspath_unsafe() +
                          File.separatorChar + cooked);
     setUrl_unsafe(board.getAttachmentsurl_unsafe() +
-                         File.separatorChar + URLEncoder.encode(cooked));
+                         File.separatorChar + UTF8URLEncoder.encode(cooked));
   }
 
+  /* (non-Javadoc)
+   * @see org.paneris.melati.boards.model.generated.AttachmentBase#setFilename(java.lang.String)
+   */
   public void setFilename(String cooked) {
     super.setFilename(cooked);
     setPath(getMessage().getBoard().getAttachmentspath() +
                          File.separatorChar + cooked);
     setUrl(getMessage().getBoard().getAttachmentsurl() +
-                         File.separatorChar + URLEncoder.encode(cooked));
+                         File.separatorChar + UTF8URLEncoder.encode(cooked));
   }
 
+  /**
+   * Ensure that duplicated filenames are made unique.
+   * 
+   * @param cooked the value to set
+   */
   public void setFilename_unique_unsafe(String cooked) {
     cooked = makeUnique(cooked);
     setFilename_unsafe(cooked);
   }
 
+  /**
+   * @param cooked
+   * @throws AccessPoemException
+   * @throws ValidationPoemException
+   */
   public void setFilename_unique(String cooked)
       throws AccessPoemException, ValidationPoemException {
     cooked = makeUnique(cooked);
     setFilename(cooked);
   }
 
- /** There are some situations where the messageboard has to deal with 
+ /** 
+  * There are some situations where the messageboard has to deal with 
   * attachments that don't have a filename!  I have no idea why, but it is 
   * bound to be microsoft's fault.
-  * if we have null, than set it to a default string.  Perhaps this
-  * should be stored in the setting table, but perhaps it's not worthwhile
+  * If we have null, than set it to a default string.  Perhaps this
+  * should be stored in the setting table, but perhaps it's not worthwhile.
   */
   public String makeUnique(String fileName) {
     if (fileName == null) fileName = "attachment";
